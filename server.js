bb87350056ec45fa2c8771c8ea62a22a1cb88e01
nobody <nobody@example.com>
2014-08-11 16:13:17 +0300

    openshift network settings

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 var debug = require('debug')('otm');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+
+var server = app.listen(app.get('port'), app.get('ip'), function() {
     debug('Express server listening on port ' + server.address().port);
 });
